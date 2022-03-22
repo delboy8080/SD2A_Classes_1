f2bec1268a71660c7eebe381add81726ee0cdfbc
delboy8080 <derek.flood@dkit.ie>
2022-03-22 16:20:02 +0000

    Added more on day Type 2

---
+++ ClockType.h
@@ -0,0 +1,27 @@
+//
+// Created by floodd on 22/03/2022.
+//
+
+#ifndef SD2A_CLASSES_1_CLOCKTYPE_H
+#define SD2A_CLASSES_1_CLOCKTYPE_H
+#include <iostream>
+using namespace std;
+class ClockType
+{
+    int hrs;
+    int mins;
+    int sec;
+public:
+    ClockType();
+    ClockType(int hh, int mm, int ss);
+
+    void increaseBySeconds(int x);
+    void increaseByMinutes(int x);
+    void increaseByHours(int x);
+    string getCurrentTime()const ;
+    void displayCurrentTime() const;
+    void resetClock();
+    void setTime(int hh, int mm, int ss);
+
+};
+#endif //SD2A_CLASSES_1_CLOCKTYPE_H
